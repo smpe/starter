76dd54677d614da3bfa9f5a69069235b0a56d49a
liudng <liudng@gmail.com>
2015-02-22 22:17:00 +0800

    Update repo.

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php
+// Copyright 2015 The Smpe Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 header('Location: public/');
